f19b42e93495f86215c450d80b55a0675286db9d
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-08-31 23:43:54 -0400

    bugfix

--- game.js
+++ game.js
@@ -301,9 +301,9 @@ createGraph();
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
     if (event.matches) {
-        equationsChart.defaults.color = "#ffffff";
+        Chart.defaults.color = "#ffffff";
     } else {
-        equationsChart.default.color = "#666666";
+        Chart.defaults.color = "#666666";
     }
 });
 
